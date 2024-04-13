4dfed3c3339e88843e465b53e01be98fab0666f5
jram828 <128335805+jram828@users.noreply.github.com>
2024-04-12 22:34:34 -0500

    CL USERDATA

--- PI-VIDEOGAMES/src/utils/crearvideojuego.js
+++ PI-VIDEOGAMES/src/utils/crearvideojuego.js
@@ -2,7 +2,7 @@ import axios from "axios";
 // import { URL } from "../App";
 
 export const crearvideojuego = async (userDataCrear) => {
-  //console.log('User data: ', userDataCrear)
+  console.log('User data: ', userDataCrear)
   const { name, description, image, launchDate, rating, platforms, genres } =
     userDataCrear;
   const newVideogame=await axios.post("/", {
